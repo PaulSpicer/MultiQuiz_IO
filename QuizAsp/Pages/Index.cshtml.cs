b7d2fb8c328d4ce888c3288e9f4e52aa616123fb
Paul Spicer <ozukki@hotmail.co.uk>
2018-08-09 12:59:18 +0100

    Added functionality to navigate to next question

--- QuizAsp/Pages/Index.cshtml.cs
+++ QuizAsp/Pages/Index.cshtml.cs
@@ -14,11 +14,11 @@ namespace QuizAsp.Pages
     {
         const string QuestionNumberKey = "QuestionNumber";
         const string AnswerCorrectKey = "AnswerCorrect";
-        const string AnswerPreviousKey = "AnswerCorrectIndex";
+        const string AnswerPreviousKey = "AnswerPreviousIndex";
 
         public string Question { get; set; }
         public string[] Answers { get; set; }
-        public int QuestionNumber { get; set; }
+        public int CurrentQuestionNumber { get; set; }
 
         public string SuccessMessage;
         public int previousAnswer = -1;
@@ -27,44 +27,61 @@ namespace QuizAsp.Pages
         {
             if (!HttpContext.Session.TryGetInt(QuestionNumberKey, out var currentQuestion))
             {
-                currentQuestion = 0;
+                CurrentQuestionNumber = 0;
+                HttpContext.Session.Set(QuestionNumberKey, BitConverter.GetBytes(CurrentQuestionNumber));
+            }
+            else
+            {
+                CurrentQuestionNumber = currentQuestion;
             }
 
             var questions = QuizCache.LoadQuiz("Quizzes/data.txt");
-            Question = questions[currentQuestion].Question;
-            Answers = questions[currentQuestion].Answers;
+            Question = questions[CurrentQuestionNumber].Question;
+            Answers = questions[CurrentQuestionNumber].Answers;
 
             if (TempData.TryGetValue(AnswerCorrectKey, out var answerCorrect))
             {
                 SuccessMessage = (bool)answerCorrect 
                     ? "Correct Answer" 
-                    : $"Incorrect Answer. The correct answer is: {questions[currentQuestion].Answers[questions[currentQuestion].CorrectAnswer]}.";
+                    : $"Incorrect Answer. The correct answer is: {questions[CurrentQuestionNumber].Answers[questions[CurrentQuestionNumber].CorrectAnswer]}.";
                 TempData.TryGetValue(AnswerPreviousKey, out var answerIndex);
                 previousAnswer = (int)answerIndex;
             }
         }
 
         [ValidateAntiForgeryToken]
-        public IActionResult OnPost (int answer)
+        public IActionResult OnPost (int answer, string answerbutton, string nextbutton)
         {
             if (!HttpContext.Session.TryGetInt(QuestionNumberKey, out var currentQuestion))
             {
-                currentQuestion = 0;
+                return new RedirectResult("/");
+            }
+            else
+            {
+                CurrentQuestionNumber = currentQuestion;
+                if (nextbutton != null)
+                {
+                    CurrentQuestionNumber++;
+                    HttpContext.Session.Set(QuestionNumberKey, BitConverter.GetBytes(CurrentQuestionNumber));
+                    return new RedirectResult("/");
+                }
             }
 
-            var questions = QuizCache.LoadQuiz("Quizzes/data.txt");
+            if (answerbutton != null)
+            {
+                var questions = QuizCache.LoadQuiz("Quizzes/data.txt");
 
-            TempData[AnswerPreviousKey] = answer;
-            TempData[AnswerCorrectKey] = (answer == questions[currentQuestion].CorrectAnswer);
-
-            HttpContext.Session.Set(QuestionNumberKey, BitConverter.GetBytes(currentQuestion));
-
+                TempData[AnswerPreviousKey] = answer;
+                TempData[AnswerCorrectKey] = (answer == questions[CurrentQuestionNumber].CorrectAnswer);
+            }
+            
             return new RedirectResult("/");
         }
     }
 
     public static class QuizCache
     {
+
         private static Dictionary<string, List<QuizQuestion>> _quizzes = new Dictionary<string, List<QuizQuestion>>();
 
         public static List<QuizQuestion> LoadQuiz (string fileName)
